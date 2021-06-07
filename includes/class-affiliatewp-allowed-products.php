994f8d2a47a531fe1626dbc0ab18988eb17d984b
dbeja <david.beja@gmail.com>
2021-06-07 07:27:20 +0100

    Add a file header to class-affiliatewp-allowed-products.php. #36

--- includes/class-affiliatewp-allowed-products.php
+++ includes/class-affiliatewp-allowed-products.php
@@ -1,23 +1,12 @@
 <?php
 /**
- * Plugin Name: AffiliateWP - Allowed Products
- * Plugin URI: https://affiliatewp.com/add-ons/official-free/allowed-products/
- * Description: Allows only specific products to generate commission
- * Author: Sandhills Development, LLC
- * Author URI: https://sandhillsdev.com
- * Version: 1.1.3
+ * Core: Plugin Bootstrap
  *
- * AffiliateWP is distributed under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * any later version.
- *
- * AffiliateWP is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ * @package     AffiliateWP Allowed Products
+ * @subpackage  Core
+ * @copyright   Copyright (c) 2021, Sandhills Development, LLC
+ * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @since       1.2
  */
 
  // Exit if accessed directly
@@ -27,11 +16,11 @@
 
  if ( ! class_exists( 'AffiliateWP_Allowed_Products' ) ) {
 
-	 /**
-	  * AffiliateWP - Allowed Products add-on.
-	  *
-	  * @since 1.0
-	  */
+	/**
+	 * Main plugin bootstrap.
+	 *
+	 * @since 1.0.0
+	 */
 	final class AffiliateWP_Allowed_Products {
 
 		/**
