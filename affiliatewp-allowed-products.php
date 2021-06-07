1126fb6f05d7db9247f11ad071ae56bb1f554103
dbeja <david.beja@gmail.com>
2021-06-07 07:25:39 +0100

    Recreate main plugin file. #36

---
+++ affiliatewp-allowed-products.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Allowed Products
+ * Plugin URI: https://affiliatewp.com/add-ons/official-free/allowed-products/
+ * Description: Allows only specific products to generate commission
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Version: 1.1.3
+ *
+ * AffiliateWP is distributed under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * AffiliateWP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ */
+
